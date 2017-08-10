bab38ddd5a73686a6a6d4a29561ebef50ff9f3df
Artem <rollmaxidom@gmail.com>
2017-08-10 15:42:53 +0200

    setuping of inputs for age and gender

--- public/app/app.js
+++ public/app/app.js
@@ -99,10 +99,15 @@ angular.module('formLogic')
         controller: function($scope){
             
 			$scope.testVar = "Appness";
-			$scope.ageRange = [...Array(100).keys()].slice(1);
-			$scope.genders = ['Male','Female'];
+			$scope.ageRange = [...Array(100).keys()].map(value => String(value)).slice(1);
+			$scope.defaultMin = $scope.ageRange[0];
+			$scope.defaultMax = $scope.ageRange.slice(-1)[0];
+			$scope.restrictMin = (age,defaultMax)=> Number(age) > Number(defaultMax);
+			$scope.restrictMax = (age,defaultMin)=> Number(age) < Number(defaultMin);
 			
-           
+			$scope.genders = ['Male','Female'];
+			$scope.selectedGender = $scope.genders[0];
+			
         }
     });
 
