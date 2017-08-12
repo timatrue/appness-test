ca06b3d2341aba8e370154e37ebe764edf701eb3
Artem <rollmaxidom@gmail.com>
2017-08-12 17:58:42 +0200

    token and account validation, improved usability of form

--- dev/app/form-logic/form-logic.component.js
+++ dev/app/form-logic/form-logic.component.js
@@ -2,17 +2,22 @@ angular.module('formLogic',['formService'])
     .component('formLogic',{
         templateUrl: './templates/form-logic.html',
         controller: function ($scope, formHttpService){
-			
+			$scope.credentials = false;
             $scope.formData = {};
 			$scope.ageRange = [...Array(100).keys()].map(value => String(value)).slice(1);
 			$scope.defaultMin = $scope.ageRange[0];
 			$scope.defaultMax = $scope.ageRange.slice(-1)[0];
 			$scope.restrictMin = (age,defaultMax)=> Number(age) > Number(defaultMax);
 			$scope.restrictMax = (age,defaultMin)=> Number(age) < Number(defaultMin);
-			$scope.genders = ['Male','Female'];
+			$scope.genders = ['All','Male','Female'];
 			$scope.selectedGender = $scope.genders[0];
+			$scope.countryValid = false;
 			
+			$scope.cleanInput = function(input){
+				formData[input] = "";
+			}
 			$scope.addCountry = function(country){
+				console.log("click", country);
 				$scope.countryQuery = country;
 				$scope.isCountryValid();
 			}
@@ -20,17 +25,43 @@ angular.module('formLogic',['formService'])
 				if($scope.countryQuery !== undefined){
 					let filtered = $scope.countryList.filter((country)=> country.toLowerCase() === $scope.countryQuery.toLowerCase());
 					$scope.formData.country = filtered.length ? filtered[0] : "";
-					$scope.showDropDown = filtered.length ? false : true;
+					$scope.countryValid = filtered.length ?  true : false;
+					if($scope.countryValid) $scope.onChange();
 				}
 			}
-
-			$scope.$watch('formData', function(){
+			
+			$scope.onChange = function(){
 				$scope.formData.min = $scope.defaultMin;
 				$scope.formData.max = $scope.defaultMax;
-				console.log('formData',$scope.formData )
-			}, true);
+				if($scope.myForm.$valid && $scope.countryValid) {
+					formHttpService.isAccountValid($scope.formData.account, $scope.formData.token)
+					.then(function(data){
+						if(data !== null) {
+							$scope.credentials = true;
+							$scope.requestAPI();
+						}
+						else $scope.credentials = false;
+					});
+				};
+			};
+			
+			
+			$scope.credentialsError = function(){
+				if($scope.myForm.$valid) {
+					if($scope.countryValid) return $scope.credentials ? "" : "Wrong Account or Token";
+				}
+				return "Fill the form";
+			}
+			$scope.requestAPI = function(){
+				let min = $scope.formData.min;
+				let max = $scope.formData.max;
+				let country = $scope.formData.country;
+				formHttpService.setParams(min, max, country);
+				console.log('requestAPI',$scope.formData, $scope.myForm.$valid)
+			}
 			$scope.countries = ()=> {
-				formHttpService.async().then(function(data) {
+				formHttpService.getCountries()
+				.then(function(data) {
 					$scope.countryData = data;
 					console.log(data)
 					$scope.countryList = data.data.map(data => data.name);
@@ -40,4 +71,3 @@ angular.module('formLogic',['formService'])
 			
         }
     });
-	
--- public/app/app.js
+++ public/app/app.js
@@ -84,17 +84,22 @@ angular.module('formLogic',['formService'])
     .component('formLogic',{
         templateUrl: './templates/form-logic.html',
         controller: function ($scope, formHttpService){
-			
+			$scope.credentials = false;
             $scope.formData = {};
 			$scope.ageRange = [...Array(100).keys()].map(value => String(value)).slice(1);
 			$scope.defaultMin = $scope.ageRange[0];
 			$scope.defaultMax = $scope.ageRange.slice(-1)[0];
 			$scope.restrictMin = (age,defaultMax)=> Number(age) > Number(defaultMax);
 			$scope.restrictMax = (age,defaultMin)=> Number(age) < Number(defaultMin);
-			$scope.genders = ['Male','Female'];
+			$scope.genders = ['All','Male','Female'];
 			$scope.selectedGender = $scope.genders[0];
+			$scope.countryValid = false;
 			
+			$scope.cleanInput = function(input){
+				formData[input] = "";
+			}
 			$scope.addCountry = function(country){
+				console.log("click", country);
 				$scope.countryQuery = country;
 				$scope.isCountryValid();
 			}
@@ -102,17 +107,43 @@ angular.module('formLogic',['formService'])
 				if($scope.countryQuery !== undefined){
 					let filtered = $scope.countryList.filter((country)=> country.toLowerCase() === $scope.countryQuery.toLowerCase());
 					$scope.formData.country = filtered.length ? filtered[0] : "";
-					$scope.showDropDown = filtered.length ? false : true;
+					$scope.countryValid = filtered.length ?  true : false;
+					if($scope.countryValid) $scope.onChange();
 				}
 			}
-
-			$scope.$watch('formData', function(){
+			
+			$scope.onChange = function(){
 				$scope.formData.min = $scope.defaultMin;
 				$scope.formData.max = $scope.defaultMax;
-				console.log('formData',$scope.formData )
-			}, true);
+				if($scope.myForm.$valid && $scope.countryValid) {
+					formHttpService.isAccountValid($scope.formData.account, $scope.formData.token)
+					.then(function(data){
+						if(data !== null) {
+							$scope.credentials = true;
+							$scope.requestAPI();
+						}
+						else $scope.credentials = false;
+					});
+				};
+			};
+			
+			
+			$scope.credentialsError = function(){
+				if($scope.myForm.$valid) {
+					if($scope.countryValid) return $scope.credentials ? "" : "Wrong Account or Token";
+				}
+				return "Fill the form";
+			}
+			$scope.requestAPI = function(){
+				let min = $scope.formData.min;
+				let max = $scope.formData.max;
+				let country = $scope.formData.country;
+				formHttpService.setParams(min, max, country);
+				console.log('requestAPI',$scope.formData, $scope.myForm.$valid)
+			}
 			$scope.countries = ()=> {
-				formHttpService.async().then(function(data) {
+				formHttpService.getCountries()
+				.then(function(data) {
 					$scope.countryData = data;
 					console.log(data)
 					$scope.countryList = data.data.map(data => data.name);
@@ -122,7 +153,7 @@ angular.module('formLogic',['formService'])
 			
         }
     });
-	
+
 
 /***/ }),
 /* 2 */
@@ -130,21 +161,63 @@ angular.module('formLogic',['formService'])
 
 angular.module('formService',[])
     .service('formHttpService', function($http){
-
-		let countryService = {
-			url:'https://graph.facebook.com/v2.10/search?type=adgeolocation&location_types=[%27country%27]&access_token=EAAcOZAxkM5moBAFZBZA5aYG2cpwwiMCBC6sYLNYATPHzCShTMApqYUV3YXtBZBbKQxY5bH21RTdMnZCEv0lDp6W3q0FZAhZCOpSVDaLIgFOCc2gj6msyqhhKBQZCd6GkqFAyChQZAbEhSKrVUnLRuiryPgJaVCk5wguH3gTaTBmNHrAZDZD&targeting_spec=%7B%22age_min%22%3A25%2C%22geo_locations%22%3A%7B%22cities%22%3A%5B%7B%22key%22%3A2430536%7D%5D%7D%7D&fields=key,name&limit=1000',
-			async: function() {
-				let promise = $http.get(this.url).then((response)=> response.data);
+		let urlManager = {
+			base: ()=> "https://graph.facebook.com/v2.10/",
+			token: ()=> "EAAcOZAxkM5moBAFZBZA5aYG2cpwwiMCBC6sYLNYATPHzCShTMApqYUV3YXtBZBbKQxY5bH21RTdMnZCEv0lDp6W3q0FZAhZCOpSVDaLIgFOCc2gj6msyqhhKBQZCd6GkqFAyChQZAbEhSKrVUnLRuiryPgJaVCk5wguH3gTaTBmNHrAZDZD",
+			tokenParam: ()=> "&access_token=",
+			account: ()=> "act_100656429",
+			search: ()=> "search?",
+			estimate: ()=> "reachestimate?",
+			cities: ()=> "type=adgeolocation&location_types=['country']&access_token=",
+			limit: ()=> "&limit=1000",
+			accessToken: ()=> "me?access_token=",
+			accessAccount: ()=> "/?fields=name",
+			
+			
+		}
+		let paramManager = {	
+			age_min: 20,
+			age_max: 60,
+			geo_locations: {countries:["AU"]},
+			flexible_spec: [{
+				work_positions: [{
+					id: 112262562119522,
+					name: "fire fighter"
+				}]
+			}]	
+		}
+		
+		let urlService = {
+			getCountries: function() {
+				let url = urlManager.base() + urlManager.search() + urlManager.cities() + urlManager.token() + urlManager.limit();
+				let promise = $http.get(url).then((response)=> response.data);
+				return promise;
+			},
+			setParams: (min, max, country)=>{
+				paramManager.age_min = min;
+				paramManager.age_max = max;
+				paramManager.geo_locations.countries = [country];
+				console.log("params",paramManager)
+				return null;
+			},
+			getFirefighters: function() {				
+				let promise = $http.get(url).then((response)=> response.data);
+				return promise;
+			},
+			isAccountValid: (account, token)=>{
+				let url = urlManager.base() + account + urlManager.accessAccount() + urlManager.tokenParam() + token;
+				console.log(url)
+				let promise = $http.get(url).then(response => response.data, error => null);
 				return promise;
 			}
 		};
-		return countryService;
+		return urlService;
     });
 	
 
+	//targeting_spec={"genders":[1,2],"age_min":20,"age_max":60,"geo_locations":{"countries":["AU"]},flexible_spec:[{"work_positions":[{"id":"112262562119522","name":"firefighter"}]}]}
+	//targeting_spec={genders:[1,2],age_min:20,age_max:60,geo_locations:{countries:["AU"]},flexible_spec:[{work_positions:[{id:112262562119522,name:firefighter}]}]}
 	
-	
-
 
 /***/ }),
 /* 3 */
